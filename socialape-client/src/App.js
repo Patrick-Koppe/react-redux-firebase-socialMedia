b21bf76b4850ab707e99fbb89bfd1d1b423cc267
Patrick Koppe <patrick.koppe@4fb.de>
2019-09-10 10:58:50 +0200

    Insert Redux, React, Login, Signup and Profile details

---
+++ socialape-client/src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData} from './redux/actions/userActions'
+// Components
+import Navbar from "./components/Navbar";
+import AuthRoute from './util/AuthRoute';
+
+// Page
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+import axios from 'axios';
+
+const theme = createMuiTheme(themeFile);
+
+const token = localStorage.FBIdToken;
+if(token) {
+  const decodeToken = jwtDecode(token);
+  // expired login token
+  if(decodeToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login';
+    store.dispatch(logoutUser())
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED});
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
+  }
+}
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+          <Router>
+              <Navbar />
+              <div className="container">
+                  <Switch>
+                      <Route exact path="/" component={home} />
+                      <AuthRoute
+                        exact
+                        path="/login"
+                        component={login}
+                      />
+                      <AuthRoute
+                        exact
+                        path="/signup"
+                        component={signup}
+                      />
+                  </Switch>
+              </div>
+          </Router>
+      </Provider>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
